e3af16629fc888076076d961d2f93940228a11c4
rzymek <krzysztof.rzymkowski@gmail.com>
2025-04-23 21:15:58 +0200

    camp dsl

--- camp-ui/src/app.tsx
+++ camp-ui/src/app.tsx
@@ -1,11 +1,14 @@
 import "./app.css"
 import {DslEditor} from "../../camp-dsl/src"
+import {useState} from "preact/compat"
 
 export function App() {
+    const [result, setResult] = useState({});
     return <div style={{display: "flex", position: "absolute", inset: 0}}>
         <DslEditor onChange={() => 0} importMetaUrl={import.meta.url}>
             .
         </DslEditor>
+        <pre></pre>
     </div>
 }
 
--- merge-template-changes.sh
+++ merge-template-changes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eu
+cd $(dirname $0)
 git subtree pull --prefix=camp-dsl/ dsl-ui-template master --squash
 git subtree pull --prefix=camp-ui/ pwa-app master --squash
 
