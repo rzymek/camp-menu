a457aaa90f1c8a0cfcdcaaccb8033882b06a6e28
rzymek <krzysztof.rzymkowski@gmail.com>
2025-05-01 22:00:12 +0200

    camp plan dsl

--- camp-dsl/test/parsing/parsing.test.ts
+++ camp-dsl/test/parsing/parsing.test.ts
@@ -3,50 +3,79 @@ import {EmptyFileSystem, type LangiumDocument} from "langium"
 import {expandToString as s} from "langium/generate"
 import {parseHelper} from "langium/test"
 import {createDslServices} from "../../src/language/dsl-module.js"
-import {Model} from "../../src/language/generated/ast.js"
+import {Count, Model} from "../../src/language/generated/ast.js"
 import {checkDocumentValid} from "../checkDocumentValid.js"
 
-let services: ReturnType<typeof createDslServices>;
-let parse:    ReturnType<typeof parseHelper<Model>>;
-let document: LangiumDocument<Model> | undefined;
+let services: ReturnType<typeof createDslServices>
+let parse: ReturnType<typeof parseHelper<Model>>
+let document: LangiumDocument<Model> | undefined
 
 beforeAll(async () => {
-    services = createDslServices(EmptyFileSystem);
-    parse = parseHelper<Model>(services.Dsl);
+    services = createDslServices(EmptyFileSystem)
+    parse = parseHelper<Model>(services.Dsl)
 
     // activate the following if your linking test requires elements from a built-in library, for example
     // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
-});
+})
 
-describe('Parsing tests', () => {
+describe("Parsing tests", () => {
 
-    test('parse simple model', async () => {
-        document = await parse(`
-            person Langium
-            Hello Langium!
-        `);
+    test("parse simple model", async () => {
+        const src: string = s`
+            czwartek (4):
+                quesadilla
+            piątek (4):
+                jajecznica z pomidorami
+                tosty
+                hamburger (5), hamburger wege (1)
+            sobota:
+                szakszuka (6)
+                curry wurst (5), curry wurst wege (1)
+                chilli con carne (5), burger wege (1)
+            niedziela (6):
+                jajecznica z pomidorami
+                tosty (5), tosty wege (1)
+                prażonka (6)
+            poniedziałek:
+                jajecznica z pomidorami (6)
+                quesadilla (5), quesadilla wege (1)
+        `
+        document = await parse(src)
 
-        // check for absence of parser errors the classic way:
-        //  deactivated, find a much more human readable way below!
-        // expect(document.parseResult.parserErrors).toHaveLength(0);
-
+        const result: Model = document.parseResult.value
         expect(
-            // here we use a (tagged) template expression to create a human readable representation
-            //  of the AST part we are interested in and that is to be compared to our expectation;
-            // prior to the tagged template expression we check for validity of the parsed document object
-            //  by means of the reusable function 'checkDocumentValid()' to sort out (critical) typos first;
-            checkDocumentValid(document) || s`
-                Persons:
-                  ${document.parseResult.value?.persons?.map(p => p.name)?.join('\n  ')}
-                Greetings to:
-                  ${document.parseResult.value?.greetings?.map(g => g.person.$refText)?.join('\n  ')}
-            `
-        ).toBe(s`
-            Persons:
-              Langium
-            Greetings to:
-              Langium
-        `);
-    });
-});
+            checkDocumentValid(document) || result.plan.reduce((acc, it) => ({
+                [`${it.day}${count(it.count)}`]: it.meals.map(meal => meal.recipies.map(it => `${it.name}${count(it.count)}`)),
+                ...acc,
+            }), {})
+            ,
+        ).toEqual({
+            "czwartek (4)": [
+                ["quesadilla"],
+            ],
+            "niedziela (6)": [
+                ["jajecznica z pomidorami"],
+                ["tosty (5)", "tosty wege (1)"],
+                ["prażonka (6)"],
+            ],
+            "piątek (4)": [
+                ["jajecznica z pomidorami"],
+                ["tosty"],
+                ["hamburger (5)", "hamburger wege (1)"],
+            ],
+            "poniedziałek": [
+                ["jajecznica z pomidorami (6)"],
+                ["quesadilla (5)", "quesadilla wege (1)"],
+            ],
+            "sobota": [
+                ["szakszuka (6)"],
+                ["curry wurst (5)", "curry wurst wege (1)"],
+                ["chilli con carne (5)", "burger wege (1)"],
+            ],
+        })
+    })
+})
 
+function count(v: Count | undefined): string {
+    return v ? "(" + v?.count + ")" : ""
+}
