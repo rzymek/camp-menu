b031375a724e8f321c6e3524e0ca8bdd7f7d94b5
rzymek <krzysztof.rzymkowski@gmail.com>
2025-05-05 21:48:54 +0200

    day of week completion

--- camp-dsl/src/language/dsl-completion-provider.ts
+++ camp-dsl/src/language/dsl-completion-provider.ts
@@ -1,25 +1,38 @@
 import {CompletionAcceptor, CompletionContext, DefaultCompletionProvider, NextFeature} from "langium/lsp"
-import {CompletionItemKind} from "vscode-languageserver-types"
 
 export class DslCompletionProvider extends DefaultCompletionProvider {
-    private items: string[] = ['initial'];
+    private items: string[] = ["initial"]
+
     override completionFor(context: CompletionContext, next: NextFeature, acceptor: CompletionAcceptor) {
         console.log("completionFor", next.type, next.property, context)
 
-        if(next.type === 'Recipe' && next.property === 'name') {
-            this.items.forEach(meal => {
-                acceptor(context, {
-                    label: meal,
-                    kind: CompletionItemKind.Text,
-                    detail: 'Keyword',
-                })
-            })
-            return;
+        if (next.type === "Recipe" && next.property === "name") {
+            this.items.forEach(meal => acceptor(context, {
+                label: meal,
+            }))
+        } else if (next.property === "day") {
+            console.log({daysOfWeek})
+            daysOfWeek.forEach((it, idx) => acceptor(context, {
+                label: it,
+                sortText: `${idx}`,
+            }))
+        } else {
+            return super.completionFor(context, next, acceptor)
         }
-        return super.completionFor(context, next, acceptor)
     }
 
     update(items: string[]): void {
-        this.items = items;
+        this.items = items
     }
 }
+
+const daysOfWeek = function () {
+    const baseDate = new Date(2023, 0, 2) // Jan 1, 2023 (a Sunday)
+    const daysOfWeek: string[] = []
+    for (let i = 0; i < 7; i++) {
+        const dayName = baseDate.toLocaleDateString(undefined, {weekday: "long"})
+        baseDate.setDate(baseDate.getDate() + 1);
+        daysOfWeek.push(dayName)
+    }
+    return daysOfWeek
+}()
