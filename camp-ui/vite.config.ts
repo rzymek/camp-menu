e451588693aef2fae864135e436e8febba6463d0
rzymek <krzysztof.rzymkowski@gmail.com>
2025-05-07 21:59:58 +0200

    base: '/camp-menu/',

--- camp-ui/vite.config.ts
+++ camp-ui/vite.config.ts
@@ -5,7 +5,7 @@ import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/camp-menu/',
     test: {
         environment: "jsdom",
         setupFiles: ["vite.setup.ts"],
